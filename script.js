07116801774de9a745fc638f7dd69accb18bed61
Robin Bergstrand <robin.bergstrand@chasacademy.se>
2022-11-20 23:14:13 +0100

    fetched and displayed language info from github repo

--- script.js
+++ script.js
@@ -35,3 +35,29 @@ async function getEmployment(url) {
 getEducation(jsonUrl);
 getStudying(jsonUrl);
 getEmployment(jsonUrl);
+
+// Getting project info
+const rockPaperScissorModal = document.querySelector("#modal-slide4");
+const rockPaperScissorLanguageList = rockPaperScissorModal.querySelector("ul");
+
+const projectUrl =
+    "https://api.github.com/repos/chasacademy-robin-bergstrand/rock-paper-scissor";
+
+async function getProjectData(projectUrl) {
+    let data = await getData(projectUrl);
+    let languages = await getData(data.languages_url);
+    console.log(languages);
+    let languageTotal = 0;
+    for (i in languages) {
+        languageTotal += languages[i];
+    }
+    for (i in languages) {
+        let percentage =
+            Math.round((languages[i] / languageTotal) * 100 * 10) / 10;
+
+        let language = i;
+        rockPaperScissorLanguageList.innerHTML += `<li><span>${language}</span>: ${percentage}%</li>`;
+    }
+}
+
+getProjectData(projectUrl);
